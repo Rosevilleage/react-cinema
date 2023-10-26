7a035504cd5a2b4c5cebf27073044969abd7c95c
rosevilleage <a65083557@gmail.com>
2023-10-26 23:50:23 +0900

    feat: Price 목업 생성

---
+++ src/components/theaterMain/PriceInfo.tsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+interface PriceOnfoProps {
+  remainSeats: number;
+  price?: number;
+  showTime?: {
+    date: string;
+    time: string;
+  };
+}
+const TOTAL_SEATS_LENGTH = 39;
+export default function PriceInfo({ remainSeats }: PriceOnfoProps) {
+  return (
+    <PriceInfoArea>
+      <p>
+        리엑트 씨네마 | 타입관 | 잔여석{" "}
+        <span className="remain">{remainSeats}</span>/{TOTAL_SEATS_LENGTH}
+      </p>
+      <p>2023.10.26(목) 14:50 ~ 17:00</p>
+      <p>
+        총 금액 <span className="price">0</span>원
+      </p>
+    </PriceInfoArea>
+  );
+}
+
+const PriceInfoArea = styled.div`
+  flex: 1 1;
+  & > p {
+    line-height: 1.6rem;
+    text-align: start;
+    font-size: 1rem;
+    > .remain {
+      color: #ff8c00;
+      font-weight: 600;
+    }
+    > .price {
+      color: #ffe100;
+      font-weight: 600;
+      text-shadow: 0 0 1px #000000;
+    }
+  }
+`;
