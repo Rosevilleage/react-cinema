9be899e1db4de3411a261cc1341fe3f07260b4d3
rosevilleage <a65083557@gmail.com>
2023-10-27 16:46:52 +0900

    feat: styled components theme 수정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,8 +3,17 @@ const color = {
   white: "#ffffff",
 };
 
+const seatBackgroundColor = {
+  general: "#12a89b",
+  handicap: "#07c400",
+  sale: "#e30101",
+  disabled: "#d3d3d3;",
+  clicked: "#0091ff",
+};
+
 export const theme = {
   color,
+  seatBackgroundColor,
 };
 
 export type Theme = typeof theme;
